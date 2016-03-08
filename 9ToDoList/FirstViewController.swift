a02475ac6ef5c9def5abbf8ff948923fd5027ab0
javiergogo <javier.go.go@hotmail.com>
2016-03-08 12:38:55 -0800

    To Do List

--- 9ToDoList/FirstViewController.swift
+++ 9ToDoList/FirstViewController.swift
@@ -8,18 +8,77 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+//declaro una variable global para los dos viewcontroller
+//para declara un arreglo vacio solo el tipo es como lo siguiente
+var toDoListArray = [String]()
 
-    override func viewDidLoad() {
+//dclaro tableview y textfield para controlar la tabla y la caja de texto para poder ocultarla
+class FirstViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate
+{
+    
+    @IBOutlet var listTable: UITableView!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //accesa a los valores de memoria y si encuentra diferente de nulo, es decir entrara si encuentra algo en memoria con el nombre toDoList
+        //como nombre de dictionary
+        if NSUserDefaults.standardUserDefaults().objectForKey("toDoList") != nil
+        {
+            //lo que encuentre en ese dictionary guardado anteriormente, lo asigna a un arreglo
+            toDoListArray = NSUserDefaults.standardUserDefaults().objectForKey("toDoList") as! [String]
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    //cuenta la cantidad de valores en arreglo y crea las filas de la tabla
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        //contara los elementos del arreglo
+        //agregara la cantidad de celdas segun el tamano del arreglo
+        return toDoListArray.count
+    }
+    
+    //asigna los valores del arreglo a las filas de la tabla
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        //agregar constante de tipo viewcell con identificador llamado Cell
+        let cel = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        //utilizar constante anterior y asignar en texto
+        //el valor del array segun el indice de la tabla
+        cel.textLabel?.text = toDoListArray[indexPath.row]
+        
+        return cel
+    }
+    
+    //funcion para actualizar cada vez que apareza este viewcontroller
+    override func viewDidAppear(animated: Bool)
+    {
+        listTable.reloadData()
+    }
+    
+    //metodo que nos permite "editar" (este caso sera eliminar) un elemento en una tabla
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath)
+    {
+        //estilo especifico cuando se desliza a la izquierda
+        if editingStyle == UITableViewCellEditingStyle.Delete
+        {
+            //borrar el elemento del arreglo en el indice indicado por la tabla
+            toDoListArray.removeAtIndex(indexPath.row)
+            
+            //actualizar lo que se guardo permanente en el telefono, ahora acutlizarlo sin el valor borrado
+            NSUserDefaults.standardUserDefaults().setObject(toDoListArray, forKey: "toDoList")
+            
+            //actualidar la lista en la tabla
+            listTable.reloadData()
+        }
+    }
+      
 }
 
--- 9ToDoList/SecondViewController.swift
+++ 9ToDoList/SecondViewController.swift
@@ -10,14 +10,44 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet var itemText: UITextField!
+    
+    @IBAction func additemButton(sender: AnyObject) {
+        //agregar datos al arreglo global
+        toDoListArray.append(itemText.text!)
+        
+        itemText.text=""
+        
+        //guardar el dato del arreglo en un dictionary llamado toDoList
+        NSUserDefaults.standardUserDefaults().setObject(toDoListArray, forKey: "toDoList")
+        
+    }
+    
+    override func viewDidLoad()
+    {
+        NSUserDefaults.standardUserDefaults().setObject(toDoListArray, forKey: "toDoList")
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        //diciendo que es verdad que la edicion(teclado) se cerrara
+        self.view.endEditing(true)
+    }
+    
+    //para que al presionar el boton return se esconda el teclado usamos esta funcion
+    func textFieldShouldReturn(textField: UITextField!) -> Bool
+    {
+        itemText.resignFirstResponder()
+        
+        return true
     }
 
 
